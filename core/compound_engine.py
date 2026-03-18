8b4a39626ce79d3a28690e278618a820006f5c49
Money Atlas <judgment007@gmail.com>
2026-03-18 20:53:05 +0700

    Create compound_engine.py

---
+++ core/compound_engine.py
@@ -0,0 +1,7 @@
+class CompoundEngine:
+    def expand(self, idea):
+        return [
+            f"{idea} under stress",
+            f"{idea} in bull market",
+            f"{idea} in crisis"
+        ]
