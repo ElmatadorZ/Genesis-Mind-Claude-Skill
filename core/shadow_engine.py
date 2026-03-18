b20acaeb683bd3c2e3ef6219012194428be38b5e
Money Atlas <judgment007@gmail.com>
2026-03-18 20:52:09 +0700

    Create shadow_engine.py

---
+++ core/shadow_engine.py
@@ -0,0 +1,7 @@
+class ShadowEngine:
+    def analyze(self, idea):
+        return [
+            "Model may be wrong",
+            "Hidden macro risk",
+            "Liquidity collapse risk"
+        ]
